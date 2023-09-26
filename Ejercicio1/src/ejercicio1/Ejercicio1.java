c615689af0ccac2a1fb9112a4854efa08dbdbe3e
Sara <saradm.zgz@gmail.com>
2023-09-26 11:57:50 +0200

    Ejercicio1 Acceso a Datos

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -5,6 +5,9 @@ public class Ejercicio1 {
 	
 	public static void main (String[] args) {
 		 File f1= new File("D\\sara\\Workspace\\ejemplosAD\\Ficheros\\escribe.txt");
+		 File f2= new File("D\\sara\\Workspace\\ejemplosAD\\Ficheros\\escribe1.txt");
+		 File f3= new File("D\\sara\\Workspace\\ejemplosAD\\Ficheros\\escribe2.txt");
+		 File f4= new File("D\\sara\\Workspace\\ejemplosAD\\Ficheros\\escribe3.txt");
 		
 	}
 	
