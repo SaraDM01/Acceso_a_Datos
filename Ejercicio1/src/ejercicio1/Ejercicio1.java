15ae1750f89a83e4b21fff42b1427f9fec194aeb
SaraDM01 <145012752+SaraDM01@users.noreply.github.com>
2023-09-26 11:51:36 +0200

    Add files via upload

---
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+import java.io.File;
+
+public class Ejercicio1 {
+	
+	public static void main (String[] args) {
+		 File f1= new File("D\\sara\\Workspace\\ejemplosAD\\Ficheros\\escribe.txt");
+		
+	}
+	
+}
