e0f6089f31af72c86d46a0789ba3919d03124f88
SaraDM01 <145012752+SaraDM01@users.noreply.github.com>
2023-10-05 13:20:06 +0200

    Add files via upload

--- ejercicios/ejercicio_datos.java
+++ ejercicios/ejercicio_datos.java
@@ -1,3 +1,16 @@
+/*Escribe un programa en Java que lea el fichero datos.txt y los guarde en otro fichero llamado tarjetas.txt con los datos de los clientes formateados de la siguiente forma:
+
+
+Nombre: Jose María
+Apellidos: Terrer Navarro
+Fecha nacimiento: 02/05/1998
+Teléfono: 654783433
+
+Nombre: Pilar
+Apellidos: Bayona Pérez
+Fecha de nacimiento: 20/02/1972
+Teléfono: 976585669*/
+
 package ejercicios;
 
 import java.io.BufferedReader;
@@ -11,48 +24,54 @@ public class ejercicio_datos {
 
 	public static void main(String[] args) {
 		
-		File mifichero = new File ("D:\\Documentos\\Sara\\Acceso_a_Datos\\Workspace\\Acceso_a_Datos\\.git\\src\\ejercicios\\datos.txt");
-		File fichero_tarjetas = new File ("D:\\Documentos\\Sara\\Acceso_a_Datos\\Workspace\\Acceso_a_Datos\\.git\\src\\ejercicios\\tarjetas.txt");
+		//donde se encuntra el fichero a leer y donde crear el nuevo
+		
+		File mifichero = new File ("src\\ejercicios\\datos.txt");
+		File fichero_tarjetas = new File ("src\\ejercicios\\tarjetas.txt");
 		
 		//leer fichero datos.txt
+		
 		try {
+			
 			FileReader lector = new FileReader(mifichero);
 			
 			BufferedReader cestaTemporal = new BufferedReader(lector);
 			String linea = cestaTemporal.readLine();
 			
-			while (linea != null) {
-				System.out.println(linea);
-				linea = cestaTemporal.readLine();
-			}
+			//Escribir fichero tarjetas.txt
 			
-			cestaTemporal.close();
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//escirbir fichero tarjetas.txt
-
-		try {
-			//mifichero.createNewFile();
 			FileWriter escritor = new FileWriter(fichero_tarjetas, true);
 			
 			InputStreamReader ir= new InputStreamReader(System.in);
 			BufferedReader br = new BufferedReader(ir);
-			String linea = br.readLine();
+			String Linea = br.readLine();
 			
-			escritor.write(linea);
+			escritor.write(Linea);
 			escritor.close();
 			
 			br.close();
 			ir.close();
 			
-		} catch (IOException e) {
+			//Separar los datos a partir de las comas
+			
+			while (linea != null) {
+
+				String[]datos =linea.split(",");
+				
+				linea = cestaTemporal.readLine();
+			}
+			
+			
+			
+			cestaTemporal.close();
+			
+			
+	} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		
+		System.out.println();
+		
 	}
 
 }
